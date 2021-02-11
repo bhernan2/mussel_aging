b877f239bddacb1472c99609e23d19130c80a4e2
Bianca A. Hernandez <bhernan2@gmail.com>
2021-02-11 17:59:31 -0600

    updated

--- page.py
+++ page.py
@@ -16,13 +16,18 @@ from datetime import datetime
 
 app = dash.Dash(external_stylesheets=[dbc.themes.LITERA])
 
+footer_height = "6rem", "10rem"
+
+# eventually move styles to a different .py file
 # the style arguments for the sidebar. We use position:fixed and a fixed width
+
+
 SIDEBAR_STYLE = {
     "position": "fixed",
     "top": 0,
     "left": 0,
     "bottom": 0,
-    "width": "28rem",
+    "width": "30rem",
     "padding": "2rem 1rem",
     "background-color": "#f8f9fa",
 }
@@ -30,26 +35,50 @@ SIDEBAR_STYLE = {
 # the styles for the main content position it to the right of the sidebar and
 # add some padding.
 CONTENT_STYLE = {
-    "margin-left": "30rem",
+    "margin-left": "32rem",
     "margin-right": "2rem",
     "padding": "2rem 1rem",
 }
 
+IMG_STYLE = {
+    # "position": "fixed",
+    #"top": 0,
+    # "left": 0,
+    # "bottom": 0,
+    "position": "absolute",
+    "bottom": "8px",
+    "width": "28rem",
+    "padding": "2rem 2rem",
+
+}
+
+FOOTER_STYLE = {
+    "position": "fixed",
+    "bottom": 0,
+    "left": 0,
+    "right": 0,
+    "height": footer_height,
+    "padding": "1rem 1rem",
+    "background-color": "#232526",
+}
+
 sidebar = html.Div(
     [
-        html.H2("Stream ecology lab: project tracking", className="display-4"),
+        html.H2("home & projects", className="display-6"),
         html.Hr(),
-        html.P(
-            "", className="lead"
-        ),
         dbc.Nav(
             [
-                dbc.NavLink("Aging", href="/", active="exact"),
                 html.Br(),
-                dbc.NavLink("Project 2", href="/page-1", active="exact"),
+                dbc.NavLink("Home", href="/", active="exact"),
                 html.Br(),
-                dbc.NavLink("Project 3", href="/page-2", active="exact"),
-                
+                dbc.NavLink("Aging", href="/page-1", active="exact"),
+                html.Br(),
+                dbc.NavLink("USACE", href="/page-2", active="exact"),
+                html.Br(),
+                dbc.NavLink("Summer", href="/page-3", active="exact"),
+                html.Br(),
+                dbc.NavLink("Zebra mussels", href="/page-4", active="exact"),
+                # html.Img(src='assets/mussel_foot.jpg', style=IMG_STYLE)
             ],
             vertical=True,
             pills=True,
@@ -60,6 +89,8 @@ sidebar = html.Div(
 
 content = html.Div(id="page-content", style=CONTENT_STYLE)
 
+footer = html.Footer(id="footer", style=FOOTER_STYLE)
+
 app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 
 
@@ -68,19 +99,67 @@ def render_page_content(pathname):
     if pathname == "/":
         return html.Div([
             dbc.Row([ 
-                html.H1("Aging project", className="display-4"),
-            ])   
-        ])
+                html.H1("Stream Ecology Project Tracking", className="display-4"),
+                html.Br(),
+                html.P("Description: This space will monitor and track the status of ongoing projects for the stream ecology lab. It will include interactive maps, charts, figures and summaries.", className="lead"),
+                ]),
+            html.Br(),
+            dbc.Container([
+                dbc.Row([
+                    dbc.Col([
+                        dbc.Card([
+                            dbc.CardBody([
+                                html.H4("Aging project"),
+                                dbc.CardImg(src="/assets/___aging.jpg"),
+                                ])
+                            ], className="flip-card-front", color="", inverse=False, style={"display": "flex"}),
+                        dbc.Card([
+                            dbc.CardBody([
+                                html.H4('Important dates', style={'font-weight': 'bold'})
+                                ]),
+                            ],className="flip-card-back", color="", inverse=False)
+                    ],className="flip-card-inner",), 
+                ],className="flip-card"),
+                dbc.Row([
+                    dbc.Col([
+                        dbc.Card([
+                            dbc.CardBody([
+                                html.H4("USACE project"),
+                                dbc.CardImg(src="/assets/_usace.png"),
+                                ])
+                            ], className="flip-card-front", color="", inverse=False, style={"display": "flex"}),
+                        dbc.Card([
+                            dbc.CardBody([
+                                html.H4('Important dates', style={'font-weight': 'bold'})
+                                ]),
+                            ],className="flip-card-back", color="", inverse=False)
+                    ],className="flip-card-inner",), 
+                ],className="flip-card"),
+                footer,
+                ],fluid=True, style={'textAlign': 'center'}),
+            ])
     elif pathname == "/page-1":
         return html.Div([
             dbc.Row([ 
-                html.H1("Project 2", className="display-4"),
+                html.H1("Aging project", className="display-4"),
             ])   
         ])
     elif pathname == "/page-2":
         return html.Div([
             dbc.Row([ 
-                html.H1("Project 3", className="display-4"),
+                html.H1("USACE", className="display-4"),
+            ])   
+        ])
+    elif pathname == "/page-3":
+        return html.Div([
+            dbc.Row([ 
+                html.H1("Summer project", className="display-4"),
+            ])   
+        ])
+    elif pathname == "/page-4":
+        return html.Div([
+            dbc.Row([ 
+                html.H1("Zebra mussels", className="display-4"),
             ])   
         ])
     # If the user tries to reach a different page, return a 404 message
