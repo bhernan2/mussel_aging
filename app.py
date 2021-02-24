2f6a2e994ae272aec9d43472b72d2f1e029cc159
Bianca A. Hernandez <bhernan2@gmail.com>
2021-02-24 15:16:17 -0600

    layout update

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ table_header = [
 row1 = html.Tr([html.Td("C(type)"), html.Td("2.0"), html.Td("632.838889"), html.Td("316.419444"),html.Td("262.279841"),html.Td("8.391599e-71"), ])
 row2 = html.Tr([html.Td("Residual"), html.Td("357.0"),html.Td("430.691667"), html.Td("1.206419"),html.Td(""),html.Td("")])
 table_body = [html.Tbody([row1, row2])]
-zebra_burrow_table1 = dbc.Table(table_header + table_body, bordered=True) 
+zebra_burrow_table1 = dbc.Table(table_header + table_body, bordered=True, size="md") 
 #footer
 footer = html.Div([
     html.Footer( html.P('Copyright © 2021 BAH - All Rights Reserved.'), id="footer", className='footer-style'),
@@ -369,14 +369,14 @@ def update_plot(value):
                                 },
                             style={'display': 'flex', 'vertical-direction': 'column',},),
                             #{ display: flex; flex-direction: column; }                      
-                        ], align="center", width=7),
+                        ], align="center", width=6),
                         #width={"sm": 12, "md": {"size": 12, "order": 6}, "lg":12},
                     dbc.Col([
                         html.Br(),
                         html.P('ANOVA', className='lead'),
                         html.Br(),
                         zebra_burrow_table1,
-                    ], align='top', width=5),
+                    ], align='top', width=6),
             ]), 
             dbc.Row([
                     dbc.Col([
@@ -392,11 +392,11 @@ def update_plot(value):
                                 },
                             style={'display': 'flex', 'vertical-direction': 'column',},),
                             #{ display: flex; flex-direction: column; }                      
-                        ], align="center", width = 7),
+                        ], align="center", width = 6),
                     dbc.Col([
                         html.P("Tukey HSD", className="lead")
-                    ], align='top', width=5),
-                        #width={"sm": 12, "md": {"size": 12, "order": 6}, "lg":12},
+                    ], align='top', width=6),
+                        #width={"sm": 12, "md": {"size": 6, "order": 3}, "lg":6},
             ]),   
         ])
     # elif value == 'plot1-info':
